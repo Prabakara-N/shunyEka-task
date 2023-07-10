2e5c7861702fb06be7da10749f9f6458a28dd5f9
Prabakaran-M <mspk028@gmail.com>
2023-07-10 15:30:18 +0530

    project setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./styles/App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5001;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} -- did not connect`));
